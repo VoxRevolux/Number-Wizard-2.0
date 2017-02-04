f63e5657c3d8eea2a02cd16f744c12c945c211b3
Leonardo <leonardo.nanci99@gmail.com>
2017-02-03 22:10:07 -0200

    Inicial Commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public void LoadLevel(string LevelName) {
+        Application.LoadLevel(LevelName);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+    int max;
+    int min;
+    int guess;
+    public int MaxGuess = 10;
+
+    Random rand = new Random();
+
+    public Text text;
+
+    // Use this for initialization
+    void Start () {
+        max = 1000;
+        min = 1;
+        NextGuess();
+	}
+	
+    public void HigherThanGuess() {
+        min = guess;
+        NextGuess();
+    }
+
+    public void LowerThanGuess() {
+        max = guess;
+        NextGuess();
+    }
+
+    public void CorrectGuess() {
+        Application.LoadLevel("Win");
+    }
+
+    public void NextGuess() {
+        guess = Random.Range(min, max+1);
+        MaxGuess--;
+
+        text.text = "Is it " + guess.ToString() + "?";
+
+        if(MaxGuess == 0 ) {
+            Application.LoadLevel("Lose");
+        }
+
+        return;
+    }
+}
